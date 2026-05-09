e13e88e2bd5b49c8894d1f5f52edf93aad6a823c
cgens67 <cgenshin587@gmail.com>
2026-05-09 11:57:20 +0800

    Update SettingsModel.kt

--- app/src/main/java/com/bnyro/clock/presentation/screens/settings/model/SettingsModel.kt
+++ app/src/main/java/com/bnyro/clock/presentation/screens/settings/model/SettingsModel.kt
@@ -1,17 +1,15 @@
-package com.bnyro.clock.presentation.screens.settings.model
+package com.clockit.cgens67.presentation.screens.settings.model
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.bnyro.clock.R
-import com.bnyro.clock.navigation.HomeRoutes
-import com.bnyro.clock.navigation.homeRoutes
-import com.bnyro.clock.util.Preferences
-import com.bnyro.clock.util.catpucchinLatte
+import com.clockit.cgens67.R
+import com.clockit.cgens67.navigation.HomeRoutes
+import com.clockit.cgens67.navigation.homeRoutes
+import com.clockit.cgens67.util.Preferences
+import com.clockit.cgens67.util.catpucchinLatte
 
 class SettingsModel : ViewModel() {
     enum class Theme(@StringRes val resId: Int) {
@@ -22,36 +20,31 @@ class SettingsModel : ViewModel() {
         SYSTEM(R.string.system), CATPPUCCIN(R.string.catppuccin)
     }
 
-    private val themeModePref =
-        Preferences.instance.getString(Preferences.themeKey, Theme.SYSTEM.name) ?: Theme.SYSTEM.name
+    var themeMode: Theme by mutableStateOf(Theme.valueOf(Preferences.instance.getString(Preferences.themeKey, Theme.SYSTEM.name) ?: Theme.SYSTEM.name))
+    var colorTheme: ColorTheme by mutableStateOf(ColorTheme.valueOf(Preferences.instance.getString(Preferences.colorThemeKey, ColorTheme.SYSTEM.name) ?: ColorTheme.SYSTEM.name))
+    var customColor by mutableStateOf(Preferences.instance.getInt(Preferences.customColorKey, catpucchinLatte.first()))
+    
+    // New Expressive Settings
+    var hapticFeedback by mutableStateOf(Preferences.instance.getBoolean("hapticFeedback", true))
+    var timeFormatOverride by mutableStateOf(Preferences.instance.getString("timeFormatOverride", "System") ?: "System")
+    var firstDayOfWeek by mutableStateOf(Preferences.instance.getString("firstDayOfWeek", "System") ?: "System")
 
-    var themeMode: Theme by mutableStateOf(Theme.valueOf(themeModePref.uppercase()))
-
-    private val colorThemePref =
-        Preferences.instance.getString(Preferences.colorThemeKey, ColorTheme.SYSTEM.name)
-            ?: ColorTheme.SYSTEM.name
-
-    var colorTheme: ColorTheme by mutableStateOf(ColorTheme.valueOf(colorThemePref.uppercase()))
-    var customColor by mutableStateOf(
-        Preferences.instance.getInt(Preferences.customColorKey, catpucchinLatte.first())
-    )
     var enabledTabs by mutableStateOf(
         homeRoutes.mapNotNull { route ->
             route.route.takeIf { Preferences.instance.getBoolean("show_tab_${route.route}", true) }
-        })
-
-    fun toggleTab(route: String, enabled: Boolean) {
-        Preferences.edit { putBoolean("show_tab_$route", enabled) }
-        val newList = homeRoutes.mapNotNull { r ->
-            r.route.takeIf { Preferences.instance.getBoolean("show_tab_${r.route}", true) }
         }
-        enabledTabs = newList
-    }
+    )
 
     var homeTab by mutableStateOf(
         homeRoutes.first {
-            it.route == Preferences.instance.getString(
-                Preferences.startTabKey, HomeRoutes.Alarm.route
-            )
-        })
+            it.route == Preferences.instance.getString(Preferences.startTabKey, HomeRoutes.Alarm.route)
+        }
+    )
+
+    fun toggleTab(route: String, enabled: Boolean) {
+        Preferences.edit { putBoolean("show_tab_$route", enabled) }
+        enabledTabs = homeRoutes.mapNotNull { r ->
+            r.route.takeIf { Preferences.instance.getBoolean("show_tab_${r.route}", true) }
+        }
+    }
 }
