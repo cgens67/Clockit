c77fd37a51ee139b602e65661be4eca6f80c82dc
cgens67 <cgenshin587@gmail.com>
2026-05-09 11:54:04 +0800

    Create Shape.kt

---
+++ app/src/main/java/com/bnyro/clock/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.clockit.cgens67.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val ExpressiveShapes = Shapes(
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(20.dp),
+    large = RoundedCornerShape(32.dp),
+    extraLarge = RoundedCornerShape(40.dp)
+)
