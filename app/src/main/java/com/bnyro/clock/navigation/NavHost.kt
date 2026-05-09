087eb6851a29fbccb4c05aa8931f75990338fdd6
cgens67 <cgenshin587@gmail.com>
2026-05-09 11:58:43 +0800

    Update NavHost.kt

--- app/src/main/java/com/bnyro/clock/navigation/NavHost.kt
+++ app/src/main/java/com/bnyro/clock/navigation/NavHost.kt
@@ -1,22 +1,22 @@
-package com.bnyro.clock.navigation
+package com.clockit.cgens67.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
+import androidx.compose.animation.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.bnyro.clock.presentation.screens.alarm.model.AlarmModel
-import com.bnyro.clock.presentation.screens.alarmpicker.AlarmPickerScreen
-import com.bnyro.clock.presentation.screens.clock.model.ClockModel
-import com.bnyro.clock.presentation.screens.permission.PermissionScreen
-import com.bnyro.clock.presentation.screens.settings.SettingsScreen
-import com.bnyro.clock.presentation.screens.settings.model.SettingsModel
-import com.bnyro.clock.presentation.screens.stopwatch.model.StopwatchModel
-import com.bnyro.clock.presentation.screens.timer.model.TimerModel
+import com.clockit.cgens67.presentation.screens.alarm.model.AlarmModel
+import com.clockit.cgens67.presentation.screens.alarmpicker.AlarmPickerScreen
+import com.clockit.cgens67.presentation.screens.clock.model.ClockModel
+import com.clockit.cgens67.presentation.screens.permission.PermissionScreen
+import com.clockit.cgens67.presentation.screens.settings.LanguagePickerScreen
+import com.clockit.cgens67.presentation.screens.settings.SettingsScreen
+import com.clockit.cgens67.presentation.screens.settings.model.SettingsModel
+import com.clockit.cgens67.presentation.screens.stopwatch.model.StopwatchModel
+import com.clockit.cgens67.presentation.screens.timer.model.TimerModel
+import com.clockit.cgens67.presentation.screens.welcome.WelcomeScreen
 
 @Composable
 fun AppNavHost(
@@ -32,21 +32,21 @@ fun AppNavHost(
     val clockModel: ClockModel = viewModel()
 
     NavHost(navController, startDestination = startDestination, modifier = modifier) {
+        
+        composable(NavRoutes.Welcome.route) {
+            WelcomeScreen {
+                navController.navigate(NavRoutes.Permissions.route) {
+                    popUpTo(NavRoutes.Welcome.route) { inclusive = true }
+                }
+            }
+        }
+        
         composable(NavRoutes.Home.route,
-            enterTransition = {
-                slideIntoContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Down,
-                    initialOffset = { it / 4 }
-                ) + fadeIn()
-            },
-            exitTransition = {
-                slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Up,
-                    targetOffset = { it / 4 }) + fadeOut()
-            }) {
+            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Down) + fadeIn() },
+            exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Up) + fadeOut() }
+        ) {
             HomeNavContainer(
-                onNavigate = {
-                    navController.navigate(it)
-                },
+                onNavigate = { navController.navigate(it) },
                 alarmModel = alarmModel,
                 clockModel = clockModel,
                 timerModel = timerModel,
@@ -55,51 +55,41 @@ fun AppNavHost(
                 settingsModel = settingsModel
             )
         }
+        
         composable(NavRoutes.Settings.route,
-            enterTransition = {
-                slideIntoContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Up,
-                    initialOffset = { it / 4 }) + fadeIn()
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Down,
-                    targetOffset = { it / 4 }) + fadeOut()
-            }) {
-            SettingsScreen(onClickBack = {
-                navController.popBackStack()
-            }, settingsModel, timerModel)
+            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Up) + fadeIn() },
+            exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Down) + fadeOut() }
+        ) {
+            SettingsScreen(
+                onClickBack = { navController.popBackStack() },
+                onNavigateLanguage = { navController.navigate(NavRoutes.LanguagePicker.route) },
+                settingsModel = settingsModel,
+                timerModel = timerModel
+            )
+        }
+        
+        composable(NavRoutes.LanguagePicker.route,
+            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Start) + fadeIn() },
+            exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.End) + fadeOut() }
+        ) {
+            LanguagePickerScreen(onBack = { navController.popBackStack() })
         }
 
         composable(NavRoutes.AlarmPicker.routeWithArgs, arguments = NavRoutes.AlarmPicker.args,
-            enterTransition = {
-                slideIntoContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Up,
-                    initialOffset = { it / 4 }) + fadeIn()
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Down,
-                    targetOffset = { it / 4 }) + fadeOut()
-            }) {
-            AlarmPickerScreen {
-                navController.popBackStack()
-            }
+            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Up) + fadeIn() },
+            exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Down) + fadeOut() }
+        ) {
+            AlarmPickerScreen { navController.popBackStack() }
         }
 
         composable(NavRoutes.Permissions.route,
-            enterTransition = {
-                slideIntoContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Up,
-                    initialOffset = { it / 4 }) + fadeIn()
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Down,
-                    targetOffset = { it / 4 }) + fadeOut()
-            }) {
+            enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Up) + fadeIn() },
+            exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Down) + fadeOut() }
+        ) {
             PermissionScreen {
-                navController.navigate(NavRoutes.Home.route)
+                navController.navigate(NavRoutes.Home.route) {
+                    popUpTo(NavRoutes.Permissions.route) { inclusive = true }
+                }
             }
         }
     }
