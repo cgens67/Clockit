85a761aa0e1dbf99ffede3ebb83a24f160091bfe
cgens67 <cgenshin587@gmail.com>
2026-05-09 15:24:28 +0800

    Update ClickableIcon.kt

--- app/src/main/java/com/clockit/cgens67/presentation/components/ClickableIcon.kt
+++ app/src/main/java/com/clockit/cgens67/presentation/components/ClickableIcon.kt
@@ -1,10 +1,12 @@
 package com.clockit.cgens67.presentation.components
 
+import android.view.HapticFeedbackConstants
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalView
 
 @Composable
 fun ClickableIcon(
@@ -13,7 +15,11 @@ fun ClickableIcon(
     contentDescription: String? = null,
     onClick: () -> Unit
 ) {
-    IconButton(modifier = modifier, onClick = onClick) {
+    val view = LocalView.current
+    IconButton(modifier = modifier, onClick = {
+        view.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK)
+        onClick()
+    }) {
         Icon(imageVector, contentDescription)
     }
 }
