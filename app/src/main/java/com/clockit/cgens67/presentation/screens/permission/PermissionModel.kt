a6e117227ba0afbfd395dce5bf159d13997428a9
cgens67 <cgenshin587@gmail.com>
2026-05-10 11:30:12 +0800

    Update PermissionModel.kt

--- app/src/main/java/com/clockit/cgens67/presentation/screens/permission/PermissionModel.kt
+++ app/src/main/java/com/clockit/cgens67/presentation/screens/permission/PermissionModel.kt
@@ -2,21 +2,32 @@ package com.clockit.cgens67.presentation.screens.permission
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.clockit.cgens67.domain.model.Permission
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 
-class PermissionModel(application: Application) :
-    AndroidViewModel(application) {
-    val requiredPermissions = allPermissions.filter {
-        !it.hasPermission(application)
-    }
+class PermissionModel(application: Application) : AndroidViewModel(application) {
+    
+    val missingPermissions: StateFlow<List<Permission>> = flow {
+        while (true) {
+            emit(allPermissions.filter { !it.hasPermission(application) })
+            delay(1000)
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = allPermissions.filter { !it.hasPermission(application) }
+    )
 
     companion object {
         val allPermissions = listOf(
             Permission.AlarmPermission,
             Permission.NotificationPermission,
-            Permission.BatteryOptimizationPermission,
-            Permission.AllDonePermission
-
+            Permission.BatteryOptimizationPermission
         )
     }
 }
